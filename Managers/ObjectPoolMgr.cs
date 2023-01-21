a3d93807986702a65f78edc7e8cdb35684721315
DOGKaiser <116735242+DOGKaiser@users.noreply.github.com>
2023-01-20 19:23:14 -0500

    remove bad usings

--- Managers/ObjectPoolMgr.cs
+++ Managers/ObjectPoolMgr.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEditorInternal.ReorderableList;
 
 public class ObjectPoolMgr {
 	private static readonly ObjectPoolMgr instance = new ObjectPoolMgr();
