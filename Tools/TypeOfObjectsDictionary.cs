c3d3b29c4659599391069a4b4e4afe68ef47dfab
DOGKaiser <116735242+DOGKaiser@users.noreply.github.com>
2023-04-30 19:51:17 -0400

    TypeOfObjectsDictionary

---
+++ Tools/TypeOfObjectsDictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TypeOfObjectsDictionary : Dictionary<Type, object> {
+	
+	public T GetTypeObject<T>() {
+		return (T)this[typeof(T)];
+	}
+	
+	public void AddTypeObject<T>(object obj) {
+		Add(typeof(T), obj);
+	}
+}
