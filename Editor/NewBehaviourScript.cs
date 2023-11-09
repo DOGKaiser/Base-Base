e518b9882b181b22491e5ad7fb57e61c795b609b
DOGKaiser <116735242+DOGKaiser@users.noreply.github.com>
2023-11-09 07:52:54 -0500

    New Skill system update

---
+++ Editor/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+
+public class MyCustomEditorWindow : OdinEditorWindow
+{
+    [MenuItem("My Game/My Editor")]
+    private static void OpenWindow()
+    {
+    }
+
+    public string Hello;
+}
